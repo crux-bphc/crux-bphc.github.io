a1ba90be4aaadd9b971f19b2ddd37a7282e0316e
Rohitt Vashishtha <aero31aero@gmail.com>
2017-03-24 02:19:46 +0530

    tools: Fix filepath bug in member-list-maker.py

--- src/maker/member-list-maker.py
+++ src/maker/member-list-maker.py
@@ -1,7 +1,7 @@
 import os
 
-MEMBER_DETAIL_LOCATION = "../docs/members"
-MEMBER_LIST_LOCATION = "../docs/members.md"
+MEMBER_DETAIL_LOCATION = "docs/members"
+MEMBER_LIST_LOCATION = "docs/members.md"
 
 files = os.listdir(MEMBER_DETAIL_LOCATION)
 member_list_file = open(MEMBER_LIST_LOCATION, "w")
@@ -9,7 +9,7 @@ member_list_file.write("# List of all Members\n\n")
 for file_name in files:
     member_name = file_name.strip(".md").replace("-", " ").title()
     member_link_line = "* " + "[" + member_name + "]" \
-                       + "(member/" + file_name + ")" + "\n"
+                       + "(members/" + file_name.strip('.md') + ".html)" + "\n"
     member_list_file.write(member_link_line)
 
 member_list_file.close()
